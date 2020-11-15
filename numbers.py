63c01b27c806d585a81859b1f06592db9a040297
Thomas Halligan <tmhalligan@gmail.com>
2020-11-15 17:26:20 -0500

    Adding content from another practice area

---
+++ numbers.py
@@ -0,0 +1,7 @@
+#print( 10 ** 5)
+
+#x = 7 ** 2 * 4 -6
+#print(x)
+
+x = 4.51
+print(round(x))
---
+++ string2.py
@@ -0,0 +1,14 @@
+first_name = 'Tom'
+last_name = 'Halligan'
+msg = first_name + ' [' + last_name + '] is a coder'
+print(msg)
+
+msg2 = f'{first_name} [{last_name}] is a coder'
+print(msg2)
+
+tom = "A really really long long strings"
+print(len(tom))
+print(tom.upper())
+print(tom.find('A'))
+
+print('long' in tom)
