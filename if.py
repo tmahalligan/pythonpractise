06670925012eb0a355433aae5b7bb929cde812db
Thomas Halligan <tmhalligan@gmail.com>
2020-11-15 20:44:11 -0500

    orginize

--- if.py
+++ if.py
@@ -1,9 +1,14 @@
-code_hot = False
+code_hot = True
 code_cold = False
 
-if code_hot:
+#if code_hot:
+#   print("Code is hot")
+#elif code_cold:
+#    print("Code is cold")
+#else:
+#    print("Something")
+
+if code_hot and not code_cold:
    print("Code is hot")
-elif code_cold:
-    print("Code is cold")
 else:
     print("Something")
